d5e9f555ef337d867e4ec0d74fbf8644966513d9
terandy <teresa.s.lacroix@gmail.com>
2025-02-20 19:52:17 -0500

    Use Loading icon from lucide-react

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,8 +1,10 @@
+import { LoaderCircle } from "lucide-react";
+
 export default function Loading({ message }: { message?: string }) {
     return (
         <div className="flex flex-col gap-3 items-center justify-center min-h-screen">
             {message && <p>{message}</p>}
-            <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-gray-900"></div>
+            <LoaderCircle className={"animate-spin rounded-full h-16 w-16 text-gray-500"} />
         </div>
     )
 }
