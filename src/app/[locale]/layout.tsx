82f3e0977ef6e71cebba066c16144bbaedd73768
teresa <teresa@imrsv.ai>
2025-03-01 19:55:40 -0500

    reorganise files for local

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,13 +1,18 @@
 import type { Metadata } from "next";
-
+import "./globals.css";
 import { cookies } from "next/headers";
 import { getCart } from "@/lib/shopify";
 import { Navbar, Footer } from "@/components";
 import { CartProvider } from "@/contexts";
-import { getMessages, setRequestLocale } from "next-intl/server";
+import { getMessages } from "next-intl/server";
 import { notFound } from "next/navigation";
 import { routing } from "@/i18n/routing";
 import { NextIntlClientProvider } from "next-intl";
+import { Geist } from "next/font/google";
+
+const geist = Geist({
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Canvas Print Shop",
@@ -22,7 +27,7 @@ interface Props {
   params: Promise<{ locale: string }>;
 }
 
-const RootLayout: React.FC<Props> = async ({ children, params }) => {
+const LocaleLayout: React.FC<Props> = async ({ children, params }) => {
   const cookiesStore = await cookies();
   const cartId = cookiesStore.get("cartId")?.value;
   const cart = await getCart(cartId);
@@ -33,21 +38,23 @@ const RootLayout: React.FC<Props> = async ({ children, params }) => {
     notFound();
   }
 
-  // Enable static rendering
-  setRequestLocale(locale);
-
   // Providing all messages to the client
   // side is the easiest way to get started
   const messages = await getMessages();
+
   return (
-    <NextIntlClientProvider messages={messages}>
-      <CartProvider cart={cart}>
-        <Navbar />
-        {children}
-        <Footer />
-      </CartProvider>
-    </NextIntlClientProvider>
+    <html lang={locale}>
+      <body className={geist.className}>
+        <NextIntlClientProvider messages={messages}>
+          <CartProvider cart={cart}>
+            <Navbar />
+            {children}
+            <Footer />
+          </CartProvider>
+        </NextIntlClientProvider>
+      </body>
+    </html>
   );
 };
 
-export default RootLayout;
+export default LocaleLayout;
