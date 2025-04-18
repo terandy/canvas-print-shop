e0a573e0b3574daa4a67bb12679546e8c5c4b4b4
teresa <teresa@imrsv.ai>
2025-04-17 21:10:10 -0400

    delete upload api route

--- src/app/api/upload/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextRequest } from "next/server";
-import { uploadImage } from "@/lib/s3/actions/image";
-
-export async function POST(request: NextRequest) {
-  return uploadImage(request);
-}
